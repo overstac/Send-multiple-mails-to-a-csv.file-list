8549cdeaa883237b1f7e6d1ac3c1dc930cfd5402
overstac <105587254+overstac@users.noreply.github.com>
2022-07-08 20:06:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import yagmail
 import os
-import time
-from datetime import datetime as dt
-
+import pandas
 sender= "pintilei92@gmail.com"
-receiver= "	andreea.nanmarinescu@yahoo.com"
-subject= "This is a great subject :D"
+subject= "Subiect"
 
-contents= "Not such a good content!"
-while True: 
-  now= dt.now()
-  if now.hour == 19 and now.minute == 40:     # TIME Z
-    yag= yagmail.SMTP(user= sender, password=os.environ['PASS'])
-    yag.send(to= receiver, subject= subject, contents= contents)
-    print("Email sent!")
-    time.sleep(60)
-    
+df = pandas.read_csv("contacts.csv")
+
+yag= yagmail.SMTP(user= sender, password=os.environ['PASS'])
+
+for index, row in df.iterrows():
+  contents= f"Hey there {row['name']} the content is nice !"
+  yag.send(to= row["email"], subject= subject, contents= contents)
+  print("Email sent!")    
